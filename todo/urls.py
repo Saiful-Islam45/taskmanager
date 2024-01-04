376b171369f597effaf825640c435be0a8e90af0
Saiful-Islam45 <saiful.islam.ice.45@gmail.com>
2024-01-04 07:00:49 +0600

    # Feature  - Add django contrib auth for user login and register  - django template for ui render  - config postgresql as a database

---
+++ todo/urls.py
@@ -0,0 +1,17 @@
+from django.urls import path
+from django.contrib.auth.views import LoginView, LogoutView
+
+from .views import (home, register, logout_user, update_todo, complete_todo,
+    delete_todo)
+
+
+urlpatterns = [
+    path("", home, name="home"),
+    path("login/", LoginView.as_view(template_name="todo/login.html"),
+        name="login"),
+    path('logout/', logout_user, name='logout'),
+    path("register/", register, name="register"),
+    path("update/todo/<int:pk>/", update_todo, name="update_todo"),
+    path("complete/todo/<int:pk>/", complete_todo, name="complete_todo"),
+    path("delete/todo/<int:pk>/", delete_todo, name="delete_todo"),
+]
